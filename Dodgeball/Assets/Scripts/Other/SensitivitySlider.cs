612a58ad66383115a0c8d029bc7f139bf467a01d
tailow <revitate@hotmail.com>
2018-08-09 19:04:13 +0300

    Added sensitivity slider

---
+++ Dodgeball/Assets/Scripts/Other/SensitivitySlider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SensitivitySlider : MonoBehaviour
+{
+    public TMP_Text sensitivityText;
+
+    PlayerMovement playerMovement;
+
+    void Start()
+    {
+        playerMovement = transform.parent.parent.parent.gameObject.GetComponent<PlayerMovement>();
+
+        sensitivityText.text = GetComponent<Slider>().value.ToString();
+    }
+
+    public void ChangeSensitivity()
+    {
+        playerMovement.sensitivity = GetComponent<Slider>().value / 10f;
+
+        sensitivityText.text = GetComponent<Slider>().value.ToString();
+    }
+}
